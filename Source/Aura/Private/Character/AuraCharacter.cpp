3b100bb41a706d2b997ebb025e0cdc7ad992c96d
steve <stevenice324@gmail.com>
2024-07-21 22:59:39 +0800

    Third Day

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,21 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	//使角色面向他们正在移动的方向
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	//设置旋转速率
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	//限制在一个特定的平面内
+	GetCharacterMovement()->bConstrainToPlane = true;
+	//当角色开始移动时，他们的初始位置会被“吸附”到bConstrainToPlane属性指定的平面上
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	//俯仰,翻滚,偏航不会根据控制器输入进行调整
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,10 +3,60 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
+	//网络同步,将玩家控制器的信息复制给客户端
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	/**
+		* ECC_Visibility是一个枚举值，表示射线投射时考虑的碰撞通道。在这里，它可能表示只考虑可见物体。
+		* 第二个参数false可能表示射线投射时不考虑物体的碰撞体积。
+		* CursorHit是传递给函数的输出参数，函数会将射线投射的结果存储在这个变量中。
+	 */
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+
+	//bBlockingHit是一个布尔值，表示射线是否与物体发生了碰撞。
+	if(!CursorHit.bBlockingHit) return ;
+	
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+	//5种情况
+	if(LastActor == nullptr)
+	{
+		if(ThisActor != nullptr)
+		{
+			ThisActor->HighlightActor();
+		}
+	} else
+	{
+		if(ThisActor == nullptr)
+		{
+			LastActor->UnHighlightActor();
+		}
+		else
+		{
+			if(LastActor != ThisActor)
+			{
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -25,3 +75,33 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
 }
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	//将InputComponent转换为UEnhancedInputComponent类型，用于更高级的输入处理
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	// 绑定一个动作到输入组件，当动作被触发时，调用Move函数
+	//参数: 动作名称 触发事件类型 表示绑定的对象,一般绑定自身 绑定的函数名称
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	// Get<Class>()是一个模板函数, 据模板参数的类型返回对应的输入值
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f,Rotation.Yaw,0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if(APawn* ControllerPawn = GetPawn<APawn>())
+	{
+		ControllerPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControllerPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
